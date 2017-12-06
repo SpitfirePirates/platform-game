060b8c8d1b0d5ae4d0147821a4015fde33c929d7
Joe Strong <joestrong@gmail.com>
2017-12-06 20:20:22 +0000

    Added ground and basic physics

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,34 +1,43 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Player from '../sprites/Player'
 
 export default class extends Phaser.State {
   init () {}
   preload () {}
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
+    this.physics.startSystem(Phaser.Physics.ARCADE)
 
-    this.mushroom = new Mushroom({
+    this.player = new Player({
       game: this,
       x: this.world.centerX,
       y: this.world.centerY,
       asset: 'mushroom'
     })
 
-    this.game.add.existing(this.mushroom)
+    this.game.add.existing(this.player)
+
+    this.game.physics.arcade.enable(this.player)
+    this.player.body.bounce.y = 0.2;
+    this.player.body.gravity.y = 300;
+    this.player.body.collideWorldBounds = true;
+
+    this.platforms = this.add.group()
+    this.platforms.enableBody = true
+
+    var ground = this.platforms.create(0, this.world.height - 32, 'platform')
+    ground.scale.setTo(24, 1)
+    ground.body.immovable = true
+  }
+
+  update () {
+    this.physics.arcade.collide(this.player, this.platforms);
   }
 
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.spriteInfo(this.player, 32, 32)
     }
   }
 }
